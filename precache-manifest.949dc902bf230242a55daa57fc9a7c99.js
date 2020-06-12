4776787775844fd8786c1cf83ad60f8b0e8e2d6c
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-06-13 04:27:50 +0530

    Updates

---
+++ precache-manifest.949dc902bf230242a55daa57fc9a7c99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07be26df02e69cf855e0ac03f5a7165",
+    "url": "/reactjs-emoji/index.html"
+  },
+  {
+    "revision": "087209c56e30f6ace3f8",
+    "url": "/reactjs-emoji/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "25e16831b147a9c9dc70",
+    "url": "/reactjs-emoji/static/css/main.a3f178a6.chunk.css"
+  },
+  {
+    "revision": "087209c56e30f6ace3f8",
+    "url": "/reactjs-emoji/static/js/2.10c65e5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-emoji/static/js/2.10c65e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e16831b147a9c9dc70",
+    "url": "/reactjs-emoji/static/js/main.51354a1b.chunk.js"
+  },
+  {
+    "revision": "0ec2109ee9ff8eea1a55",
+    "url": "/reactjs-emoji/static/js/runtime-main.70b07d78.js"
+  }
+]);
